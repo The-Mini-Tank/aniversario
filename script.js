d6e7c22560ac527b132dfc0c13ad6c88e51df21e
The Mini-Tank <96495394+The-Mini-Tank@users.noreply.github.com>
2024-05-04 01:37:23 -0300

    Add files via upload
    Ultimo update.

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-var cliques = 0;
 var imagem = document.getElementById("imagem");
 var texto = document.getElementById("texto");
+var texto2 = document.getElementById("texto2");
+var audio = document.getElementById("audio");
+var cliques = 0;
+var animacaoAtiva = false;
+
+function mudarSrcTemporariamente(novoSrc, duracao) {
+    var srcOriginal = imagem.src;
+    imagem.src = novoSrc;
+
+    setTimeout(function() {
+        imagem.src = srcOriginal;
+        animacaoAtiva = false;
+    }, duracao);
+
+    animacaoAtiva = true;
+}
+
+function mudarSrcDefinitivamente(novoSrc) {
+    imagem.src = novoSrc;
+}
 
 imagem.addEventListener("click", function() {
-    cliques++;
-    if (cliques >= 3) {
-        texto.style.display = "block";
-        imagem.style.display = "none"; // Faz a imagem desaparecer
+    if (!animacaoAtiva && cliques < 3) {
+        cliques++;
+        if (cliques === 3) {
+            mudarSrcDefinitivamente("gif_win.gif");
+            texto.style.display = "block"; 
+            texto2.style.display = "block";
+            audio.volume = 0.4;
+            audio.play();
+            confetti();
+        } else {
+            mudarSrcTemporariamente("gif_click.gif", 1000); 
+        }
     }
 });
